ae9fe33f4904517ede1d2ef0dd6818e64b00f0ec
yasminsel <ye5494892@gmail.com>
2023-09-08 13:26:15 -0700

    commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Description: print all digit letters
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		printf("%i" . digit);
+		digit++;
+	}
+	printf("\n");
+
+	return (0);
+}
