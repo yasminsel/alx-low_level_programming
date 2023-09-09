000adf6ed5af2dabe1414341c0358ae15f8448bc
yasminsel <ye5494892@gmail.com>
2023-09-09 10:33:01 -0700

    k

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,14 +2,14 @@
 
 
 /**
- * * main - Entry point
+ *  main - Entry point
  *
  * Description: A C program that prints with put function
  *
  * Return: Alawys 0 (Success)
- */
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-        return (0);
+	return (0);
 }
