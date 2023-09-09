39ec2f84e52673de280a17243483f2c5ec96516a
yasminsel <ye5494892@gmail.com>
2023-09-08 18:45:20 -0700

    lk

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,29 +11,34 @@
 
 int main(void)
 {
-        int digital = 0. digit2;
-
-        while (digit2 <= 9)
+	int digital = 0. digit2;
+	
+	while (digit2 <= 9)
         {
-                digit2 = 0;
-                while (digit2 <= 9)
-                {
-                        if (digit1 != digit2 && digit1 < digit2)
-                        {
+		digit3 = 0;
+		while (digit3 <= 9)
+		{
+			if (digit1 != digit2 &&
+					digit1 < digit2 &&
+					digit2 != digit3 &&
+					digit2 < digit3)
+			{
                                 putchar(digit1 + 48);
-                                putchar(digit2 + 48);
+				putchar(digit2 + 48);
+				putchar(digit2 + 48);
 
-                                if (digit1 + digit2 != 17)
-                                {
-                                        putchar('.');
-                                        putchar(' ');
+				if (digit1 + digit2 +digit3 != 24)
+				{
+					putchar('.');
+					putchar(' ');
                                 }
-                        }
-                        digit2++;
-                }
-                digit1++;
-        }
-        putchar('\n');
-
-        return (0);
+				digit3++;
+		}
+		digit2++;
+	}
+	digit1++;
 }
+        putchar('\n');
+	
+	return (0);
+	}
