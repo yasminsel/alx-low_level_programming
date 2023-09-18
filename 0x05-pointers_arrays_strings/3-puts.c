6c19c587fb3c5cdd43d338e8d66a22b9dfec99ab
yasminsel <ye5494892@gmail.com>
2023-09-18 06:34:21 -0700

    a

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - function that print string
+ *
+ * @str: string parameter
+ *
+ * Return: Nothing
+*/
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*s + 0);
+		str++;
+	}
+	_putchar('\n');
+}
