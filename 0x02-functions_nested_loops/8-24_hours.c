e4d46b7e64ff04385e7def10fb076f79d6e9a017
yasminsel <ye5494892@gmail.com>
2023-09-18 13:43:00 -0700

    k

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,11 +12,11 @@ void jack_bauer(void)
 	{
 		for (min = 0; min <= 59; main++)
 		{
-			_putchar(hr / 10) + 48);
-			_putchar(hr % 10) + 48);
+			_putchar((hr / 10) + 48);
+			_putchar((hr % 10) + 48);
 			_putchar(':');
-			_putchar(min / 10) + 48);
-			_putchar(min % 10) + 48);
+			_putchar((min / 10) + 48);
+			_putchar((min % 10) + 48);
 			_putchar('\n');
 		}
 	}
