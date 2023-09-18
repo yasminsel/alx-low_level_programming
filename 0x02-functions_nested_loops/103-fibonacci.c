6db1de9f5a017e301aa9bf7e85c8e85f3c4034e0
yasminsel <ye5494892@gmail.com>
2023-09-18 14:11:57 -0700

    kj

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -20,13 +20,14 @@ int main(void)
 
 		if (sum > 4000000)
 			break;
+
 		if ((sum % 2) == 0)
 			total_sum += sum;
 
 		fib1 = fib2;
 		fib2 = sum;
 	}
-	printf("%,0f\n", total_sum);
+	printf("%.0f\n", total_sum);
 
 	return (0);
 }
