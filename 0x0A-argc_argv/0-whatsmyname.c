5d249c252554d57c7efc8ca76bba0e4961043d42
yasminsel <yasminseleem010@gmail.com>
2023-10-27 12:53:08 -0700

    gh

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints program name, followed by line
+ * @argc: int
+ * @argv: liat
+ * Return: 0
+*/
+
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("@s\n", argv[0]);
+	return (0);
+}
