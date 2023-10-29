867b4049b92ee417fc064796822e832428569017
yasminsel <yasminseleem010@gmail.com>
2023-10-29 14:50:56 -0700

    SD

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,13 +13,13 @@ int main(int argc, char *argv[])
 
 	if (argc == 3)
 	{
-		sum = atoi(argv[1]) * atoi(argv[2]);
-		printf("%d\n", sum);
+		sum = atoi(argv[1] * atoi(argv[2]);
+				printf("%d\n", sum);
 	}
 	else
 	{
-		printf("Error\n");
-		return (1);
+	printf("Error\n");
+	return (1);
 	}
 	return (0);
 }
