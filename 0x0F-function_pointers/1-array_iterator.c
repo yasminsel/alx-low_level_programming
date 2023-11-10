b732c156a9d06ef867b27b20b8c7d5a7fc1ad25a
yasminsel <yasminseleem010@gmail.com>
2023-11-10 03:32:37 -0800

    S

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,16 @@
+#include "function_pointer.h"
+
+/**
+ * array_iterator - maps an array
+ * @array: the int array
+ * @size: the array size
+ * @action: function pointer
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int *end = array + size - 1;
+
+	if (array && size && action)
+		while (array <= end)
+			action(*array++);
+}
