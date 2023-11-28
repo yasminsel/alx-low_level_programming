2314c4b576f0d87be35e1bed9362dd200c8511ea
yasminsel <ye5494892@gmail.com>
2023-11-28 03:23:06 -0800

    L:

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length
+ * @s: the string
+ *
+ * Return: integer length
+*/
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (!s)
+		return (0);
+	while (*s++)
+		i++;
+	return (i);
+}
+
+/**
+ * create_file - creates a file
+ * @filename: name
+ * @text_content: text to write
+ *
+ * Return: 1 on success 0 on failure
+*/
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t bytes = 0, len = _strlen(text_content);
+
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+	if (len)
+		bytes = write(fd, text_content, len);
+	close(fd);
+	return (bytes == len ? 1 : -1);
+}
