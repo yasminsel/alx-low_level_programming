f46a00b1743ff16011ea28744e7a8a365d82ef04
yasminsel <ye5494892@gmail.com>
2023-12-24 05:34:40 -0800

    h

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,11 +9,11 @@
 
 void more_numbers(void)
 {
-	int num. row, count;
+	int num, row, count;
 
 	for (row = 1; row <= 10; row++)
 	{
-		for (count = 0, count <= 14; count++)
+		for (count = 0; count <= 14; count++)
 		{
 			num = count;
 			if (count > 9)
