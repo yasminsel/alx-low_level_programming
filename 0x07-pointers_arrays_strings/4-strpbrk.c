a5380ca21c74c528fb23f7013b33b420a5c4ed37
yasminsel <ye5494892@gmail.com>
2023-12-26 10:39:07 -0800

    L

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strpbrk - search a string
+ * @s: string
+ * @accept: string to match
+ * Return: pointer
+*/
+char *_strpbrk(char *s, char *accept)
+{
+	int itr, jtr;
+	char *p;
+
+	itr = 0;
+	while (s[itr] != '\0')
+	{
+		jtr = 0;
+		while (accept[jtr] != '\0')
+		{
+			if (accept[jtr] == s[itr])
+			{
+				p = &s[itr];
+				return (p);
+			}
+			jtr++;
+		}
+		itr++;
+	}
+
+	return (0);
+}
