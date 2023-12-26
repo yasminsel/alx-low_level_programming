750a2f97ebb609dac18ac7f156f7fac876ecf01e
yasminsel <ye5494892@gmail.com>
2023-12-26 10:24:18 -0800

    d

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length
+ *
+ * @s: string
+ * @accept: bytes
+ * Return: unsigned int
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int itr, jtr;
+
+	for (itr = 0; s[itr] != '\0'; itr++)
+	{
+		for (jtr = 0; accept[jtr] != s[itr], jtr++)
+		{
+			if (accept[jtr] == '\0')
+				return (itr);
+		}
+	}
+	return (itr);
+}
+
